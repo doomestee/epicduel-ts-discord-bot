eb7aa5e2a6b6d3afdbb8b2a682cb8943d0435ee0
Doomester <doomester@duck.com>
2024-04-23 00:34:38 +0100

    fix text

--- src/events/epicduel/adminMessage.ts
+++ src/events/epicduel/adminMessage.ts
@@ -239,14 +239,14 @@ export default new EDEvent("onAdminMessage", async function (hydra, obj) {
 
                     let text:string = `**${list.length}** bombs were dropped from <t:${Math.floor(lastTime/1000)}:T> to <t:${Math.floor(lastBomb.overall.time / 1000)}:T>.\n\nStat:\n\n`;
 
-                    text += `* Overall:\n  * **Bomb Per Second**: ${list.length/((lastBomb.overall.time-list[0].time)/1000)} bomb(s)\n  * **Bomb Per Minute**: ${list.length/((lastBomb.overall.time-list[0].time)/1000/60)} bomb(s).\n`;
+                    text += `* Overall:\n  * **Bomb Per Second**: ${Math.round(list.length/((lastBomb.overall.time-list[0].time)/10))/100} bomb(s)\n  * **Bomb Per Minute**: ${Math.round(list.length/((lastBomb.overall.time-list[0].time)/10/60))/100} bomb(s).\n`;
 
-                    if (lastBomb.exile) text += `\n* Exile:\n  * **Total Bomb**: ${stat.count.exile}\n  * **Bomb Per Second**: ${list.length/((lastBomb.exile.time-list[0].time)/1000)} bomb(s)\n  * **Bomb Per Minute**: ${list.length/((lastBomb.exile.time-list[0].time)/1000/60)} bomb(s).\n`;
-                    if (lastBomb.legion) text += `\n* Legion:\n  * **Total Bomb**: ${stat.count.legion}\n  * **Bomb Per Second**: ${list.length/((lastBomb.legion.time-list[0].time)/1000)} bomb(s)\n  * **Bomb Per Minute**: ${list.length/((lastBomb.legion.time-list[0].time)/1000/60)} bomb(s).\n`;
+                    if (lastBomb.exile) text += `\n* Exile:\n  * **Total Bomb**: ${stat.count.exile}\n  * **Bomb Per Second**: ${Math.round(list.length/((lastBomb.exile.time-list[0].time)/10))/100} bomb(s)\n  * **Bomb Per Minute**: ${Math.round(list.length/((lastBomb.exile.time-list[0].time)/10/60))/100} bomb(s).\n`;
+                    if (lastBomb.legion) text += `\n* Legion:\n  * **Total Bomb**: ${stat.count.legion}\n  * **Bomb Per Second**: ${Math.round(list.length/((lastBomb.legion.time-list[0].time)/10))/100} bomb(s)\n  * **Bomb Per Minute**: ${Math.round(list.length/((lastBomb.legion.time-list[0].time)/10/60))/100} bomb(s).\n`;
 
                     // TODO: biggest bomber by count by score etc for both  side as well
 
-                    text += `\nBiggest bomber:\n* By Count: **${sorted.byCount[0]}** with ${sorted.byCount[1]} points given.\n* By Score: **${sorted.byScore[0]}** with ${sorted.byScore[1]} score.`;
+                    text += `\nBiggest bomber:\n* By Count: **${sorted.byCount[0]}** with ${sorted.byCount[1]} bombs.\n* By Score: **${sorted.byScore[0]}** with ${sorted.byScore[1]} influence.`;
 
                     text += `\n\n${Object.keys(stat.user).length} unique warriors achieved ${stat.score.overall} influence.`;
 
